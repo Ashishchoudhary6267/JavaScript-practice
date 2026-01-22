cce8faae8b80f61db23ad4da43a7fad8a20041c1
ashish kumar <ak9068020@gmail.com>
2026-01-22 13:24:26 +0530

    conditional statement practice

---
+++ conditional.js
@@ -0,0 +1,135 @@
+// conditionl statements #####
+//falsy : false , 0 , "" , null , undefined , NaN
+//truthy : 1 , -1 , "ashish" , [] , {}, "0", " ", function(){}
+
+// if( " ")
+// {
+//     console.log("truthy value");
+// }
+// else{
+//     console.log("falsy value");
+// }
+// if("")
+// {
+//     console.log("truthy value" );
+// }
+// else{
+//     console.log("falsy value");
+// }
+// if([])
+// {
+//     console.log("truthy value" );   
+// }else{
+//     console.log("falsy value");
+// }
+
+// console.log([] == false);
+
+// use elseuf for multiple conditions #####
+
+// let age = 17;
+
+// if(age >= 19){
+//     console.log("you can vote");
+// }
+// else if(age >= 18){
+//     console.log("you can vote");
+// }
+// else{
+//     console.log("you can't vote");
+// }
+// use else if method for grading system
+// can we use here a operator ?
+// and operator &&
+// or operator |
+// for all the grades above 50 pass else fail make all the grades a ,b ,c ,d ,e, fail ###
+
+// let marks = 76;
+// if(marks >= 50 && marks < 60){  
+//     console.log("E grade");
+// }
+// else if(marks >= 60 && marks < 70){
+//     console.log("D grade");
+// }
+// else if(marks >= 70 && marks < 80){
+//     console.log("C grade");
+// }
+// else if(marks >= 80 && marks < 90){
+//     console.log("B grade");
+// }
+// else if(marks >= 90){
+//     console.log("A grade");
+// }
+// else{
+//     console.log("F grade");
+// }
+
+// trafic signal  ####
+// let signal = "yellow";
+
+// if(signal === "red"){
+//     console.log("stop");
+// }
+// else if(signal === "yellow"){
+//     console.log("wait");
+// }
+// else{
+//     console.log("go");
+// }
+
+// //switch stagement
+// let day = 40;
+// switch(day){
+//     case 1:
+//         console.log("monday");
+//         break;
+//     case 2:
+//         console.log("tuesday");
+//         break;
+//     case 3:
+//         console.log("wednesday");
+//         break;
+//     case 4:
+//         console.log("thursday");
+//         break;
+//     default:
+//         console.log("invalid day");
+// }
+// // trafic light with switch statement
+// let signal2 = "green";
+// switch(signal2){
+//     case "red":
+//         console.log("stop");
+//         break;
+//     case "yellow":
+//         console.log("wait");
+//         break;
+//     default:
+//         console.log("go");
+//}
+
+
+// whenn evere run the dice it should give a random number between 1 to 6  with math.random()  and math.floor()  and switchstatement  ###
+const dice = Math.floor(Math.random() * 6) + 1;
+
+switch(dice){
+    case 1:
+        console.log("you rolled 1");
+        break;
+    case 2 :
+        console.log("you rollled  2");
+        break;
+    case 3:
+        console.log("you rolled  3");
+        break;
+    case 4:
+        console.log("you rolled  4");
+        break;
+    case 5:
+        console.log("you rolled  5");
+        break;
+    case 6:
+        console.log("you rolled  6");
+        break;
+
+}
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,8 @@
-console.log(Number("23a"));
+Number.prototype.ashish_randomBetween = function(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+// const num5 = 0;
+// console.log(num5.ashish_randomBetween(23, 100));
+// console.log(Object.getOwnPropertyNames(Number.prototype));
+
+// console.log(num5);
